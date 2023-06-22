3eba9efc0b5a2db16c9ce131b4f7963b431d0e61
philbosch <philippmartinbosch@gmail.com>
2023-06-22 13:59:46 +0000

    adapt script

--- scripts/geocode.R
+++ scripts/geocode.R
@@ -1,6 +1,10 @@
 ### packages
 library(dplyr)
 library(stringr)
+library(sf)
+library(tigris)
+
+options(tigris_class = "sf")
 
 ### read in data
 
@@ -56,3 +60,51 @@ oh_2016_geocoded <- oh_2016 %>%
 
 vroom::vroom_write(oh_2012_geocoded, file = "data/raw_polling_places/oh_2012_geocoded.tsv")
 vroom::vroom_write(oh_2016_geocoded, file = "data/raw_polling_places/oh_2016_geocoded.tsv")
+
+
+
+# Fetch the boundaries for Ohio
+ohio <- states() %>%
+  filter(STUSPS == "OH")
+
+# Ensure your data frame's coordinates are in a suitable format for sf
+oh_2012_geocoded_sf <- st_as_sf(oh_2012_geocoded, coords = c("long", "lat"), crs = 4326)
+
+# Transform data frame to the same CRS as the Ohio boundaries
+oh_2012_geocoded_sf <- st_transform(oh_2012_geocoded_sf, st_crs(ohio))
+
+# Check if points are within Ohio
+in_ohio <- st_within(oh_2012_geocoded_sf, ohio)
+
+# Add results back to the data frame
+oh_2012_geocoded <- oh_2012_geocoded %>%
+  mutate(In_Ohio = lengths(in_ohio) > 0)
+
+# manually fix wrong geocodes
+st_mary <- c(39.906476723022465, -81.2295872435207)
+sharon_woods <- c(39.27799137521471, -84.40322339195049)
+american_legion <- c(41.42958722192429, -83.84391309239182)
+
+oh_2012_geocoded %>%
+  mutate(lat = case_when(
+    In_Ohio == FALSE & str_detect(name, "ST MARYS CHURCH HALL") ~ 39.906476723022465
+  )) %>% View()
+
+# for 2016
+# Ensure your data frame's coordinates are in a suitable format for sf
+oh_2016_geocoded_sf <- st_as_sf(oh_2016_geocoded, coords = c("long", "lat"), crs = 4326)
+
+# Transform data frame to the same CRS as the Ohio boundaries
+oh_2016_geocoded_sf <- st_transform(oh_2016_geocoded_sf, st_crs(ohio))
+
+# Check if points are within Ohio
+in_ohio <- st_within(oh_2016_geocoded_sf, ohio)
+
+# Add results back to the data frame
+oh_2016_geocoded <- oh_2016_geocoded %>%
+  mutate(In_Ohio = lengths(in_ohio) > 0)
+
+
+# manually fix wrong geocodes
+st_mary <- c(39.906476723022465, -81.2295872435207)
+
